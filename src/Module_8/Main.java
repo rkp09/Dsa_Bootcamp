ab0ea27bd89401909492395561c9390756560bfe
Ridoy Chandra Dey <ridoychandradey09@gmail.com>
2022-09-20 10:17:09 +0600

    Initial setup and Module are added

---
+++ src/Module_8/Main.java
@@ -0,0 +1,13 @@
+package Module_8;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("what is your name? ");
+        String name = input.next();
+        System.out.println(name);
+
+    }
+}
---
+++ src/Module_8/Sum.java
@@ -0,0 +1,13 @@
+package Module_8;
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
+        int sum = first + second;
+        System.out.println("Sum = " + sum);
+    }
+}
