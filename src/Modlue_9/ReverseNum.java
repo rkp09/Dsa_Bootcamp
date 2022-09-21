435b90e7e76326cd758dd1d1c116404b24d01630
Ridoy Chandra Dey <ridoychandradey09@gmail.com>
2022-09-21 19:48:55 +0600

    ReverseNum.java completed using moduler method

--- src/Modlue_9/ReverseNum.java
+++ src/Modlue_9/ReverseNum.java
@@ -7,14 +7,22 @@ public class ReverseNum {
         // Q: Reverse the number in the input
         Scanner in = new Scanner(System.in);
         long n = in.nextLong();
+//        String strNum = String.valueOf(n);
+//        int len = strNum.length();
+//        String reverse = "";
+//        for (int i = len - 1; i >= 0; i--){
+//            reverse += strNum.charAt(i);
+//        }
+//
+//        System.out.println(Integer.parseInt(reverse));
 
-        String strNum = String.valueOf(n);
-        int len = strNum.length();
-        String reverse = "";
-        for (int i = len - 1; i >= 0; i--){
-            reverse += strNum.charAt(i);
+        long reverse = 0;
+        while (n > 0){
+            long digit = n % 10;
+            reverse = reverse * 10 + digit;
+            n /= 10;
         }
 
-        System.out.println(Integer.parseInt(reverse));
+        System.out.println(reverse);
     }
 }
